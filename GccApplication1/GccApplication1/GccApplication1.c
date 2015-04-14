59ccae7dd79dfa1c4b51195295b438ecd3c94e7c
johantennisplayer <johan_hakansson@comhem.se>
2015-04-14 14:34:57 +0200

    AtmelProject

---
+++ GccApplication1/GccApplication1/GccApplication1.c
@@ -0,0 +1,17 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2015-04-14 14:32:32
+ *  Author: Johan Håkansson
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
